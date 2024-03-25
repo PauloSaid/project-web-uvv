ec7f750cb03c2ba190aaac9e1e298737d7d14c7b
alunodev02 <alunodev02@lab.uvv>
2024-03-25 08:46:21 -0300

    Update web app -> Route categories and register implemented

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 /** @type{import('fastify').FastifyPluginAsync<>} */
 export default async function auth(app, options) {
     
+    const users = app.mongo.db.collection('users');
+
+
     app.post('/auth', (request, reply) => {
         let user = request.body;
         request.log.info(`Login for user ${user.username}`);
@@ -9,5 +12,28 @@ export default async function auth(app, options) {
         return {
             'x-access-token' : app.jwt.sign(user)
         }
+    });
+    
+    app.post('/register', {
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    id: { type: 'integer' },
+                    username: { type: 'string' },
+                    passwd: { type: 'string' }
+                },
+                required: ['username', 'passwd']
+            }
+        },
+        config: {
+            requireAuthentication: false
+        }
+    }, async (request, reply) => {
+        let user = request.body;
+        
+        await users.insertOne(user);
+
+        return reply.code(201).send();
     });
 }
---
+++ routes/categories.js
@@ -0,0 +1,78 @@
+/** @type{import('fastify').FastifyPluginAsync<>} */
+import createError from '@fastify/error';
+export default async function categories(app, options) {
+    const InvalidCategoryError = createError('InvalidCategoryError', 'Categoria Inválida.', 400);
+
+    const categories = app.mongo.db.collection('categories');
+
+    app.get('/categories', 
+        {
+            config: {
+                logMe: true
+            }
+        }, 
+        async (request, reply) => {
+            return await categories.find().toArray();
+        }
+    );
+
+    app.post('/categories', {
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    id: { type: 'integer' },
+                    name: { type: 'string' },
+                    imgUrl: { type: 'string' }
+                },
+                required: ['name', 'imgUrl']
+            }
+        },
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let category = request.body;
+        
+        await categories.insertOne(category);
+
+        return reply.code(201).send();
+    });
+
+    app.get('/categories/:id', async (request, reply) => {
+        let id =  request.params.id;
+        let category = await categories.findOne({_id: new app.mongo.ObjectId(id)});
+        
+        return category;
+    });
+    
+    app.delete('/categories/:id', {
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let id =  request.params.id;
+        
+        await categories.deleteOne({_id: new app.mongo.ObjectId(id)});
+        
+        return reply.code(204).send();;
+    });
+
+    app.put('/categories/:id', {
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let id =  request.params.id;
+        let category = request.body;
+        
+        await categories.updateOne({_id: new app.mongo.ObjectId(id)}, {
+            $set: {
+                name: category.name,
+                imgUrl: category.imgUrl
+            }
+        });
+        
+        return reply.code(204).send();;
+    });
+}
